6b86b22ef8b37b6ed03c6df2f9870a528e661a18
ring1996 <ring199623@gmail.com>
2021-11-18 00:36:16 +0900

    [add] Todoの削除機能を実装

--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,12 +1,21 @@
 import classes from "./TaskList.module.scss";
 
 export const TaskList = (props) => {
+  const onClickDeleteTodo = (index) => {
+    const newTodos = [...props.todos];
+    newTodos.splice(index, 1);
+    props.setTodos(newTodos);
+  };
   return (
     <div className={classes.inner}>
       <ul>
         {props.todos.map((todo, index) => {
           return (
-            <li key={index} className={classes.item}>
+            <li
+              key={index}
+              className={classes.item}
+              onClick={() => onClickDeleteTodo(index)}
+            >
               <span>{todo}</span>
               <i className="far fa-trash-alt"></i>
             </li>
